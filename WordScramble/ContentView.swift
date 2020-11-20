10170aed1d6b4b46d3c756474fa4e1882f7a5d20
Juan Dorado <fdorado985@gmail.com>
2020-11-19 23:16:02 -0600

    Create a function to check if the function exists

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -70,6 +70,14 @@ struct ContentView: View {
 
     return true
   }
+
+  func isReal(word: String) -> Bool {
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+    return misspelledRange.location == NSNotFound
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
